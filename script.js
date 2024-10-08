9122c9f351a1c53cb3a2b0ab6011e53b088c8a3f
Inna Manush <manush.inna@gmail.com>
2024-10-08 16:46:46 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//first task
+//
+// let count=0;
+// for(let i=20;i<30;i++){
+//     count=i+0.5
+//     console.log(count);
+// }
+//
+//second task
+//
+// let bucks=26;
+// for(uan=10;uan<=100;uan++){
+//      let uanInBucks=uan*bucks;
+//      console.log(uanInBucks);
+// }
+//
+//third task
+//
+// let number = prompt("Give me some number")
+// number=Number(number);
+// for( let i=1;i<100;i++){
+//     if(i*i<number){
+//         console.log(i);
+//     }
+// }
+//
+//forth task
+//
+// let primeNumber= prompt("Give me some prime number" );
+// primeNumber=Number(primeNumber);
+// if (primeNumber <= 1) {
+//     console.log("I asked for a prime number!");
+// }else {
+//     let isPrime= true;
+//
+//     for(let i= 2; i < primeNumber; i++) {
+//         if (primeNumber % i === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//
+//     if(isPrime){
+//         console.log("Thank you!" + primeNumber + " is a prime number.");
+//     }else {
+//         console.log("I asked for a prime number!");
+//     }
+// }
